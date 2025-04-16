2d19ef5dfb1e133b394a6051471d823d19301e28
Bora <bora@christinesmbp16.mynetworksettings.com>
2025-04-16 14:08:59 -0400

    Your commit message

--- index.js
+++ index.js
@@ -5,3 +5,25 @@
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+function calculateTax(amount) {
+    return amount * 0.10;
+  }
+  console.log(calculateTax(100)); // Should print 10
+
+  function convertToUpperCase(text) {
+    return text.toUpperCase();
+  }
+ 
+  function findMaximum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+  }
+  function isPalindrome(word) {
+    const cleaned = word.toLowerCase();
+    const reversed = cleaned.split('').reverse().join('');
+    return cleaned === reversed;
+  }
+  function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discount = (originalPrice * discountPercentage) / 100;
+    return originalPrice - discount;
+  }
